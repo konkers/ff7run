8a711b36e13c94f38c17bc5fe9840099a8d5654a
Erik Gilling <konkers@konkers.net>
2020-01-07 23:01:29 -0800

    Setup firebase hosting.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCMSCC02dy4jO_gKhFfRD4FG4lyl-rNh78',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: '<your-database-URL>',
+    projectId: 'luckyseven-ba2d5',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
+  },
 };
