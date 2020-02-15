22d7b1c00c857ea22fc1a27300a7c69c8859bcb8
Erik Gilling <konkers@konkers.net>
2020-02-15 02:45:25 +0000

    wip

--- shared/data.ts
+++ shared/data.ts
@@ -4,6 +4,7 @@ import * as materia from "./materia.json";
 import * as characters from "./characters.json";
 import * as jobs from "./jobs.json";
 
-export const MATERIA_LIST: Materia[] = <Materia[]>materia["default"];
+
+export const MATERIA_LIST: Materia[] = <Materia[]>(materia["default"]);
 export const CHARACTER_LIST: string[] = <string[]>characters["default"];
 export const JOB_LIST: Job[] = <Job[]>jobs["default"];
--- shared/model.ts
+++ shared/model.ts
@@ -33,3 +33,22 @@ export interface Run {
   ty: RunType;
   job_data?: JobRunData;
 }
+
+export interface RunConfig {
+  ty: RunType;
+}
+
+// Two copies of the run are maintianed.  `full` is the complete, generated
+// run while `current` is the current state of the run visable to the runner.
+// As the runner unlocks more of the run (e.x. by aquiring a character) data
+// from `full` to `current`.  This makes run generation easier while keeping
+// the fun mechanic of unlocking as you go.
+//
+// `config` should be accessable to the user.
+//
+// Database rules are needed to enforce visibility here.
+export interface StoredRun {
+  config: RunConfig;
+  full: Run;
+  current: Run;
+}
