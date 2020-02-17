288e31e3a7da412a94caa03d146fa4027ad2e076
Erik Gilling <konkers@konkers.net>
2020-02-17 01:23:36 -0800

    Improve navigation flow.

--- src/app/generate/generate.component.spec.ts
+++ src/app/generate/generate.component.spec.ts
@@ -1,8 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { from } from 'rxjs';
-
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireFunctions } from '@angular/fire/functions';
