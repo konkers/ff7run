5761d02b0955e3a3e3c53a07331698c9c7155933
Erik Gilling <konkers@konkers.net>
2020-02-17 01:44:54 -0800

    Make run list look better.

--- src/app/run-list/run-list.component.ts
+++ src/app/run-list/run-list.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs';
 
-import { RunState } from '@shared';
+import { RunState, character_list } from '@shared';
 
 import { RunsService } from '../runs.service';
 
@@ -26,5 +26,24 @@ export class RunListComponent implements OnInit {
 
   gotoRun(run: string) {
     this.router.navigate([`/run/${run}`]);
+  }
+
+  formatDate(date: Date): string {
+    const opt = {
+      weekday: 'short',
+      year: 'numeric',
+      month: 'short',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    };
+    return date.toLocaleDateString(undefined, opt);
+  }
+
+  getUnlockedChars(run: RunState): string {
+    const names = character_list()
+      .map(c => c.name)
+      .filter(name => name in run.data.job_data.jobs);
+    return names.join(', ');
   }
 }
