8badf74e4af9d52655677d1e4dbb6f115607e069
Erik Gilling <konkers@konkers.net>
2020-03-12 13:53:01 -0700

    Move backend to a unified endpoint.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,6 +9,7 @@ import {
   RunState,
   RunType,
   UnlockJobCommand,
+  ApiCommand,
   character_list,
 } from '../../projects/shared/src/public-api';
 
@@ -34,92 +35,112 @@ function plans(db, context) {
   return db.collection(`runs/plan/${context.auth.uid}`);
 }
 
-export const newRun = functions.https.onCall(
-  async (config: RunConfig, context) => {
-    if (!context.auth) {
-      throw new functions.https.HttpsError(
-        'permission-denied',
-        'Must be logged in to create a run.'
-      );
-    }
+async function handleNewRun(config: RunConfig, context: any): Promise<string> {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      'permission-denied',
+      'Must be logged in to create a run.'
+    );
+  }
 
-    if (config.ty !== RunType.Job) {
-      throw new functions.https.HttpsError(
-        'invalid-argument',
-        `Invalid run type ${config.ty}`
-      );
-    }
+  if (config.ty !== RunType.Job) {
+    throw new functions.https.HttpsError(
+      'invalid-argument',
+      `Invalid run type ${config.ty}`
+    );
+  }
 
-    const [plan, state] = jobRunGen.newRun(config);
+  const [plan, state] = jobRunGen.newRun(config);
 
-    // Hack to work around Timestamp class mismatch between frontend and backend.
-    // We should refactor new_job_run() and friends to take a timestamp.
-    const now = admin.firestore.Timestamp.now();
-    state.timestamp = now;
-    state.log[0].when = now;
+  // Hack to work around Timestamp class mismatch between frontend and backend.
+  // We should refactor new_job_run() and friends to take a timestamp.
+  const now = admin.firestore.Timestamp.now();
+  state.timestamp = now;
+  state.log[0].when = now;
 
-    const db = admin.firestore();
-    const ref = await states(db, context).add(state);
+  const db = admin.firestore();
+  const ref = await states(db, context).add(state);
 
-    // Write back state with its id;
-    state.id = ref.id;
+  // Write back state with its id;
+  state.id = ref.id;
+  await states(db, context)
+    .doc(ref.id)
+    .set(state);
+
+  await plans(db, context)
+    .doc(ref.id)
+    .set(plan);
+
+  return ref.id;
+}
+
+async function handleUnlockJob(cmd: UnlockJobCommand, context: any) {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      'permission-denied',
+      'Must be logged in to create a run.'
+    );
+  }
+
+  if (!(cmd.name in charMap)) {
+    throw new functions.https.HttpsError(
+      'invalid-argument',
+      `Invalid character name ${cmd.name}`
+    );
+  }
+
+  const db = admin.firestore();
+  const state = (
     await states(db, context)
-      .doc(ref.id)
-      .set(state);
+      .doc(cmd.run_id)
+      .get()
+  ).data() as RunState;
 
+  if (cmd.name in state.data.job_data.jobs) {
+    return;
+  }
+
+  const plan = (
     await plans(db, context)
-      .doc(ref.id)
-      .set(plan);
+      .doc(cmd.run_id)
+      .get()
+  ).data() as RunData;
 
-    return ref.id;
+  state.data.job_data.jobs[cmd.name] = plan.job_data.jobs[cmd.name];
+  const now = admin.firestore.Timestamp.now();
+  state.timestamp = now;
+  state.log.push({
+    when: now,
+    message: `Unlocked ${cmd.name}.`,
+  });
+
+  await states(db, context)
+    .doc(cmd.run_id)
+    .set(state);
+
+  return;
+}
+
+export const command = functions.https.onCall(
+  async (cmd: ApiCommand, context): Promise<any> => {
+    if (cmd.newRun) {
+      return await handleNewRun(cmd.newRun, context);
+    } else if (cmd.unlockJob) {
+      await handleUnlockJob(cmd.unlockJob, context);
+    }
   }
 );
 
+// Deprecated endpoint for legacy clients.
+export const newRun = functions.https.onCall(
+  async (config: RunConfig, context) => {
+    return await handleNewRun(config, context);
+  }
+);
+
+// Deprecated endpoint for legacy clients.
 export const unlockJob = functions.https.onCall(
   async (cmd: UnlockJobCommand, context) => {
-    if (!context.auth) {
-      throw new functions.https.HttpsError(
-        'permission-denied',
-        'Must be logged in to create a run.'
-      );
-    }
-
-    if (!(cmd.name in charMap)) {
-      throw new functions.https.HttpsError(
-        'invalid-argument',
-        `Invalid character name ${cmd.name}`
-      );
-    }
-
-    const db = admin.firestore();
-    const state = (
-      await states(db, context)
-        .doc(cmd.run_id)
-        .get()
-    ).data() as RunState;
-
-    if (cmd.name in state.data.job_data.jobs) {
-      return;
-    }
-
-    const plan = (
-      await plans(db, context)
-        .doc(cmd.run_id)
-        .get()
-    ).data() as RunData;
-
-    state.data.job_data.jobs[cmd.name] = plan.job_data.jobs[cmd.name];
-    const now = admin.firestore.Timestamp.now();
-    state.timestamp = now;
-    state.log.push({
-      when: now,
-      message: `Unlocked ${cmd.name}.`,
-    });
-
-    await states(db, context)
-      .doc(cmd.run_id)
-      .set(state);
-
-    return;
+    await handleUnlockJob(cmd, context);
   }
 );
